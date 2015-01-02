922b5bc83413f65c9ffdeb2791725903cfb2f438
digitalG2 <jblynn2@gmail.com>
2015-01-02 11:15:29 -0700

    Modifying Things to Try exercise

--- ch2_numbers/ch2_things_to_try.rb
+++ ch2_numbers/ch2_things_to_try.rb
@@ -3,3 +3,12 @@
 puts "There are #{365 * 24} hours in a year."
 puts "There are #{365 * 10 * 24 * 60} minutes in a decade."
 puts "My age in seconds is #{30 * 365 * 24 * 60 * 60}."
+
+#Bonus - calculate the author's age when he finished the book 1.160 million seconds to years
+seconds = 1160000000
+minutes = seconds / 60
+hours = minutes / 60
+days = hours / 24
+years = days / 365
+
+puts "The author was #{years} at the time of finishing his book."
