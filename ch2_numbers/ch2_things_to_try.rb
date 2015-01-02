c5b3b2170c4543b80ed7005121cf1a99441f599a
digitalG2 <jblynn2@gmail.com>
2015-01-01 22:17:44 -0700

    Changed folder structure

---
+++ ch2_numbers/ch2_things_to_try.rb
@@ -0,0 +1,5 @@
+#Chapter 2
+#Things to try
+puts "There are #{365 * 24} hours in a year."
+puts "There are #{365 * 10 * 24 * 60} minutes in a decade."
+puts "My age in seconds is #{30 * 365 * 24 * 60 * 60}."
